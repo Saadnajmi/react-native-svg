e91e94da95ec109b79125ac37fa39b440262abcb
Saad Najmi <sanajmi@microsoft.com>
2022-03-30 16:32:28 -0700

    wip

---
+++ Example/react-native.config.js
@@ -0,0 +1,31 @@
+const project = (() => {
+  const fs = require("fs");
+  const path = require("path");
+  try {
+    const {
+      androidManifestPath,
+      iosProjectPath,
+      windowsProjectPath,
+    } = require("react-native-test-app");
+    return {
+      android: {
+        sourceDir: "android",
+        manifestPath: androidManifestPath(path.join(__dirname, "android")),
+      },
+      ios: {
+        project: iosProjectPath("ios"),
+      },
+      windows: fs.existsSync("windows/example.sln") && {
+        sourceDir: "windows",
+        solutionFile: "example.sln",
+        project: windowsProjectPath(path.join(__dirname, "windows")),
+      },
+    };
+  } catch (_) {
+    return undefined;
+  }
+})();
+
+module.exports = {
+  ...(project ? { project } : undefined),
+};
